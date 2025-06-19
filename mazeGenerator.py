398e29ef269a99e713f8eb1c9f5d436d67ae8281
xuanz421 <lowkaiu999@gmail.com>
2025-06-19 11:40:49 +0800

    git commit -m "Fix: Ensure maze generation works"

--- mazeGenerator.py
+++ mazeGenerator.py
@@ -1,12 +1,17 @@
 import random
 
+
 def difficultSetup():
     difficulty = int(input("Choose your difficulty(1~5)"))
     return difficulty
 
 difficulty = difficultSetup()
 WIDTH, HEIGHT = (difficulty * 10 + 1), (difficulty * 6 + 5)
-
 WALL, PATH = 0, 1
 
-wall = [[WALL for _ in range(WIDTH)] for _ in range(HEIGHT)]
+maze = [[WALL for _ in range(WIDTH)] for _ in range(HEIGHT)]
+
+
+
+def carve(x,y):
+    maze[x][y]= PATH
